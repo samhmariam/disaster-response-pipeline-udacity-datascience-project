9f0b011281a93351b4bb90f35210e409646ff60c
Samuel H.Mariam <samhmariam@gmail.com>
2024-08-17 20:52:13 +0100

    initial commit from local repo

---
+++ data/process_data.py
@@ -0,0 +1,95 @@
+'''
+
+    - To run ETL pipeline that cleans data and stores in 
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - Arguments:
+        - messages_filepath: path to csv file containing messages
+        - categories_filepath: path to csv file containing categories
+        - database_filepath: path to SQLite database to save clean data to
+    
+    - Output:
+        - SQLite database containing a merged and cleaned version of the messages and categories data
+
+    
+'''
+
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+
+def load_data(messages_filepath, categories_filepath):
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+
+    return df
+
+
+def clean_data(df):
+        
+        # create a dataframe of the 36 individual category columns
+        categories = df['categories'].str.split(';', expand=True)
+        
+        # select the first row of the categories dataframe
+        row = categories.iloc[0]
+        
+        # use this row to extract a list of new column names for categories
+        category_colnames = row.apply(lambda x: x[:-2])
+        
+        # rename the columns of `categories`
+        categories.columns = category_colnames
+        
+        # convert category values to just numbers 0 or 1
+        for column in categories:
+            categories[column] = categories[column].apply(lambda x: x[-1])
+            categories[column] = categories[column].astype(int)
+        
+        # drop the original categories column from `df`
+        df.drop('categories', axis=1, inplace=True)
+        
+        # concatenate the original dataframe with the new `categories` dataframe
+        df = pd.concat([df, categories], axis=1)
+        
+        # drop duplicates
+        df.drop_duplicates(inplace=True)
+        
+        return df
+
+
+def save_data(df, database_filename):
+    
+    engine = create_engine('sqlite:///./data/' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
